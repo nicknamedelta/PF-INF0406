2e2512c6662e4464761a83665ea5cc459d10aaa0
nicknamedelta <evaldo.felipe2@gmail.com>
2023-02-14 18:23:58 -0300

    add: duplicated unique field and foreignKey errors

--- backend/src/errors/CreateError.ts
+++ backend/src/errors/CreateError.ts
@@ -1,13 +1,23 @@
 import { HttpStatus } from "@nestjs/common";
+import { NON_MAPPED_ERRORS, PRISMA_ERRORS } from "./constants";
 
 export const CreateError = (error, context) => {
+    let description;
+
+    const { code, meta } = error || {};
+    const { target: fields } = meta || {};
+
+    if (error.code && PRISMA_ERRORS.hasOwnProperty(error.code)) {
+        description = `${PRISMA_ERRORS[`${error.code}`]}`;
+    }
+
     return {
         status: HttpStatus.BAD_REQUEST,
-        error: `Cannot create the ${context}. See the details to understand this error.`,
+        error: `Cannot create the ${context}. See the details below to understand this error.`,
         details: {
-            code: error.code,
-            version: error.clientVersion,
-            fields: error.meta.target,
+            code,
+            fields,
+            description: description || NON_MAPPED_ERRORS,
             context,
         },
     };
---
+++ backend/src/errors/constants.ts
@@ -0,0 +1,7 @@
+export const PRISMA_ERRORS = {
+    P2002: "Unique constraint already exists on table",
+    P2003: "ForeignKey doesnt exists in the reference table",
+};
+
+export const NON_MAPPED_ERRORS = "Invalid data passed through request.";
+
